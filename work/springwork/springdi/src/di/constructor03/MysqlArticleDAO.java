194245effaf49db69e0210d7cc48a77f7e15aa8b
Myomi-Yoo <myomi0618@gmail.com>
2020-01-23 17:45:00 +0900

    DI(setter)&Annotation&JDBC

---
+++ work/springwork/springdi/src/di/constructor03/MysqlArticleDAO.java
@@ -0,0 +1,9 @@
+package di.constructor03;
+
+public class MysqlArticleDAO implements IArticleDAO{
+	@Override
+	public void insert(ArticleDTO article) {
+		System.out.println("Mysql");
+	}
+
+}
--- work/springwork/springdi/src/dl/exam01/TV.java
+++
@@ -1,8 +0,0 @@
-package dl.exam01;
-
-public interface TV {
-	void turnOn();
-	void turnOff();
-	void soundUp();
-	void soundDown();
-}
