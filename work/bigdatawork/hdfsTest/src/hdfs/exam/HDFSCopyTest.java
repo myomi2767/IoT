7ba1bdb1a2fbce1bf419132bc755b68c87bf09d0
Myomi-Yoo <myomi0618@gmail.com>
2020-02-19 17:43:46 +0900

    mapreduce

--- work/bigdatawork/hdfsTest/src/hdfs/exam/HDFSCopyTest.java
+++ work/bigdatawork/hdfsTest/src/hdfs/exam/HDFSCopyTest.java
@@ -10,22 +10,27 @@ import org.apache.hadoop.fs.Path;
 
 public class HDFSCopyTest {
 	public static void main(String[] args) {
-		//1. hdfs를 제어하기 위해서 설정 파일을 읽어서 사용해야 하므로
-		//	 hadoop 설치 폴더의 설정 파일을 접근하기 위해 제공되는 클래스
-		Configuration conf = new Configuration();
-		//2. HDFS를 접근하기 위해서 제공되는 객체생성 - HDFS객체
-		FileSystem hdfs = null;
-		//3. HDFS로부터 input하기 위해 스트림객체
 		
-		FSDataInputStream hdfsin = null;
-		FSDataOutputStream hdfsout = null;
+		Configuration conf = new Configuration();
+		FileSystem hdfs = null;
+		
+		FSDataInputStream hdfsIn = null;
+		FSDataOutputStream hdfsOut = null;
 		try {
 			hdfs = FileSystem.get(conf);
-			Path path1 = new Path(args[0]);
-			Path path2 = new Path(args[1]);
-			hdfsin = hdfs.open(path1);
-			hdfsout = hdfs.create(path2);
-			hdfsout.writeUTF(hdfsin.readUTF());
+			Path inPath = new Path(args[0]);
+			Path outPath = new Path(args[1]);
+			hdfsIn = hdfs.open(inPath);
+			hdfsOut = hdfs.create(outPath);
+			while(true) {
+				int data = hdfsIn.read();
+				System.out.println((char)data);
+				if(data==-1) {
+					break;
+				}
+				hdfsOut.write((char)data);
+			}
+			// hdfsOut.writeUTF(hdfsIn.readUTF());
 			
 		} catch (IOException e) {
 			e.printStackTrace();
