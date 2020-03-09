43f462795bc84d4fe4aa9432c3456da44415daf1
Myomi-Yoo <myomi0618@gmail.com>
2020-03-09 17:45:29 +0900

    Bigdata(Sort)

---
+++ work/bigdatawork/advancedMapReduce/src/mapreduce/air/sort/AirSortMapper.java
@@ -0,0 +1,29 @@
+package mapreduce.air.sort;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class AirSortMapper extends Mapper<LongWritable, Text, CustomKey, IntWritable> {
+	static final IntWritable outputVal = new IntWritable(1);
+	CustomKey outputKey = new CustomKey(); //output key
+	//output value
+	@Override
+	protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, CustomKey, IntWritable>.Context context)
+			throws IOException, InterruptedException {
+		if (key.get() > 0) {
+			String[] line = value.toString().split(",");
+			if (line != null & line.length > 0) {
+				if (!line[15].equals("NA") && Integer.parseInt(line[15]) > 0) {
+					outputKey.setYear(line[0]);
+					outputKey.setMonth(new Integer(line[1]));
+					context.write(outputKey, outputVal);
+				}
+			}
+		}
+	}
+
+}
