07264b95aa513b563aebb7093492d2fc3813d840
myomi2767 <myomi0618@gmail.com>
2020-05-08 17:06:47 +0900

    Multi Chatting2&Android Multi Chat

---
+++ work/network/multichat/src/chat/step08/ClientChatListener.java
@@ -0,0 +1,22 @@
+package chat.step08;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class ClientChatListener implements ActionListener{
+	ClientChatView view;
+
+	public ClientChatListener(ClientChatView view) {
+		super();
+		this.view = view;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//1. 채팅 대화를 전송하는 경우 프로토콜, 대화내용, nickname 
+		if(e.getSource()==view.txtinput | e.getSource()==view.btnsend) {
+			view.sendMsg("chatting/"+view.txtinput.getText().trim()+"/"+view.nickname);
+			view.txtinput.setText("");
+		}
+		
+	}
+	
+	
+}
